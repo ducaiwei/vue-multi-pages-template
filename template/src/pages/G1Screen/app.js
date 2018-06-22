5713b765615a6c03c1864ffe25d695d9180a3a22
unknown <062764170@hdjt.hdad.local>
2018-06-22 15:20:47 +0800

    ''

---
+++ template/src/pages/G1Screen/app.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './app'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
